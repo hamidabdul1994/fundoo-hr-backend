36997abc5015ac752ac403250f6483c344cffdcd
dilip-bridgelabz <dilip-bridgelabz@github.com>
2017-03-27 10:58:58 +0530

    Commiting the git conflicts

--- config/index.js
+++ config/index.js
@@ -1,26 +1,58 @@
-// var _ = require("lodash");
-// var defaults = require("./default.js");
-// var config = require("./" + (process.env.NODE_ENV || "development") + ".js");
-// module.exports = _.merge({}, defaults, config);
+var winston = require('winston'),
+    dateFormat = require('dateformat'),
+    clc = require('cli-color');
+winston.emitErrs = true;
+
+var mapping = {
+    log: clc.blue,
+    warn: clc.yellow,
+    error: clc.red.bold,
+    info: clc.cyan
+};
+
+["log", "warn", "error", "info"].forEach(function(method) {
+    var oldMethod = console[method].bind(console);
+    console[method] = function() {
+        oldMethod.apply(
+            console, [mapping[method](dateFormat(new Date(), "ddd, mmm d yyyy h:MM:ss TT Z")), mapping[method](method), ':']
+            .concat(mapping[method](arguments[0]))
+        );
+    };
+});
 
 var config = {
     production: {
-        host: 'domain.com',
-        port: 3030,
+        host: '',
+        port: 80,
         session: {
             key: 'the.express.session.id',
             secret: 'something.super.secret'
         },
-        database: 'mongodb://<user>:<pwd>@apollo.modulusmongo.net:27017/db',
+        database: 'mongodb://<user>:<pwd>@mongodb.host.net:27017/db',
         twitter: {
             consumerKey: 'consumer Key',
             consumerSecret: 'consumer Secret',
             callbackURL: 'http://yoururl.com/auth/twitter/callback'
-        }
+        },
+        logger: new winston.Logger({
+            transports: [
+                new winston.transports.File({
+                    filename: '/var/log/server.log',
+                    level: 'error'
+                }),
+                new winston.transports.Console({
+                    level: 'debug',
+                    handleExceptions: true,
+                    json: false,
+                    colorize: true
+                })
+            ],
+            exitOnError: false
+        })
     },
     development: {
         host: 'localhost',
-        port: 3030,
+        port: process.env.NODE_PORT || 3030,
         session: {
             key: 'the.express.session.id',
             secret: 'something.super.secret'
@@ -30,11 +62,37 @@ var config = {
             consumerKey: 'consumer Key',
             consumerSecret: 'consumer Secret',
             callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+        },
+        logger: new winston.Logger({
+            transports: [
+                new winston.transports.File({
+                    level: 'info',
+                    filename: './logs/all-logs.log',
+                    handleExceptions: true,
+                    json: true,
+                    maxsize: 5242880, //5MB
+                    maxFiles: 5,
+                    colorize: false
+                }),
+                new winston.transports.Console({
+                    level: 'debug, error',
+                    handleExceptions: true,
+                    json: true,
+                    colorize: true
+                })
+            ],
+            exitOnError: false,
+            emitErrs: true
+        }),
+        stream: {
+            write: function(message, encoding) {
+                this.logger.info(message);
+            }
         }
     },
     default: {
         host: 'localhost',
-        port: 3030,
+        port: process.env.NODE_PORT || 3030,
         session: {
             key: 'the.express.session.id',
             secret: 'something.super.secret'
@@ -44,10 +102,35 @@ var config = {
             consumerKey: 'consumer Key',
             consumerSecret: 'consumer Secret',
             callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+        },
+        logger: new winston.Logger({
+            transports: [
+                new winston.transports.File({
+                    level: 'info',
+                    filename: './logs/all-logs.log',
+                    handleExceptions: true,
+                    json: true,
+                    maxsize: 5242880, //5MB
+                    maxFiles: 5,
+                    colorize: false
+                }),
+                new winston.transports.Console({
+                    level: 'debug,error',
+                    handleExceptions: true,
+                    json: true,
+                    colorize: true
+                })
+            ],
+            exitOnError: false,
+            emitErrs: true
+        }),
+        stream: {
+            write: function(message, encoding) {
+                this.logger.info(message);
+            }
         }
     }
 }
-
 
 exports.get = function get(env) {
     return config[env] || config.default;
